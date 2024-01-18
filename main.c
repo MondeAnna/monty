d205dd6433b5595300d69c4fa71f5ab84391fc3c
MondeAnna <monde.anna@gmail.com>
2024-01-18 13:11:47 +0200

    w.i.p -> open and close file

--- main.c
+++ main.c
@@ -15,6 +15,26 @@ void check_cli_args(int argc)
 }
 
 /**
+ * _open - open bytecode file
+ * @file_name: name of file
+ * Descriptioin: It is the responsibility of the
+ * caller to close the file
+ * Return: pointer to file
+ */
+FILE *_open(char *file_name)
+{
+	FILE *file = fopen(file_name, "r");
+
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		exit(EXIT_FAILURE);
+	}
+
+	return (file);
+}
+
+/**
  * main - entry point
  * @argc: arg count
  * @argv: arg values
@@ -22,12 +42,13 @@ void check_cli_args(int argc)
  */
 int main(int argc, char **argv)
 {
-	char *script_name;
+	FILE *file;
 
 	check_cli_args(argc);
 
-	script_name = argv[1];
-	puts(script_name);
+	file = _open(argv[1]);
+
+	fclose(file);
 
 	return (EXIT_SUCCESS);
 }
