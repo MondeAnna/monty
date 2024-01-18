34b35d3553c3f893e76347de5e8241e7183ad6b6
MondeAnna <monde.anna@gmail.com>
2024-01-18 19:53:55 +0000

    trimwhite space

--- main.c
+++ main.c
@@ -1,42 +1,38 @@
 #include "monty.h"
 
 
-
-
-
-// Stores the trimmed input string into the given output buffer, which must be
-// large enough to store the result.  If it is too small, the output is
-// truncated.
 size_t trimwhitespace(char *out, size_t len, const char *str)
 {
-if(len == 0)
-return 0;
+	const char *end;
+	size_t out_size;
 
-const char *end;
-size_t out_size;
+	if(len == 0)
+		return 0;
 
-// Trim leading space
-while(isspace((unsigned char)*str)) str++;
 
-if(*str == 0)  // All spaces?
-{
-*out = 0;
-return 1;
-}
+	while (isspace((unsigned char) *str))
+		str++;
 
-// Trim trailing space
+	if (*str == 0)  // All spaces?
+	{
+		*out = 0;
+		return (1);
+	}
+
 	end = str + strlen(str) - 1;
-	while(end > str && isspace((unsigned char)*end)) end--;
+
+	while (end > str && isspace((unsigned char) *end))
+		end--;
+
 	end++;
 
-// Set output size to minimum of trimmed string length and buffer size minus 1
 	out_size = (end - str) < len-1 ? (end - str) : len-1;
 
-// Copy trimmed string and add null terminator
 	memcpy(out, str, out_size);
+
 	out[out_size] = 0;
 
-	return out_size;
+	return (out_size);
 }
 
 
