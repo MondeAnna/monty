f18b2fa28b64070f467bab328081c9374f4272ab
MondeAnna <monde.anna@gmail.com>
2024-01-19 05:53:35 +0200

    make cmd_init betty complient

--- cmd_init.c
+++ cmd_init.c
@@ -6,13 +6,13 @@
  */
 cmd_t *cmd_init()
 {
-    cmd_t *cmd = malloc(sizeof(cmd_t));
+	cmd_t *cmd = malloc(sizeof(cmd_t));
 
-    cmd->file = NULL;
-    cmd->line = NULL;
-    cmd->opcode = NULL;
-    cmd->value = NULL;
-    cmd->line_number = 0;
+	cmd->file = NULL;
+	cmd->line = NULL;
+	cmd->opcode = NULL;
+	cmd->value = NULL;
+	cmd->line_number = 0;
 
-    return (cmd);
+	return (cmd);
 }
