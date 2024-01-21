1bb31e588b2c019e54a31898aadcd57e6643e0aa
MondeAnna <monde.anna@gmail.com>
2024-01-21 14:01:00 +0200

    `cmd_init` -> add null check

--- cmd_init.c
+++ cmd_init.c
@@ -8,6 +8,12 @@ cmd_t *cmd_init()
 {
 	cmd_t *cmd = malloc(sizeof(cmd_t));
 
+	if (!cmd)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
 	cmd->file = NULL;
 	cmd->line = NULL;
 	cmd->opcode = NULL;
